ad7f906ee3501f06923a1e60293ce33fecf0e3ae
parisakh4 <126240144+parisakh4@users.noreply.github.com>
2024-09-06 17:35:10 +0200

    for loops

--- app.py
+++ app.py
@@ -1,29 +1,6 @@
-from turtledemo.penrose import start
-
-command = ""
-started = False
-while True:
-    command = input(">").lower()
-    if command == "start":
-        if started:
-            print("The car has already started.")
-        else:
-            started = True
-            print("Car Started")
-    elif command == "stop" :
-        if not started:
-            print("The car is already stopped!")
-        else:
-            started = False
-            print("Car stopped.")
-    elif command == "help":
-        print ("""
-start - to start the car
-stop - to stop the car
-quit - to quit
-        """)
-    elif command == "quit":
-        break
-    else:
-        print("Sorry I do not understand that")
-
+numbers = [2,2,2,2,5]
+for x_count in numbers:
+     output = ''
+     for count in range(x_count):
+         output += 'x'
+     print(output)
