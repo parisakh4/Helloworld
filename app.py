fe1b41b6d3b0369da9ac9f408e9e869d3cb53abc
parisakh4 <126240144+parisakh4@users.noreply.github.com>
2024-09-06 13:15:06 +0200

    while

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-weight = int(input('Enter your Weight'))
-unit = input('(L)bs or (K)g').upper()
+from turtledemo.penrose import start
 
-if unit == "L"  or unit == "LBS" :
-    converted = weight * 0.45
-    print(f'You are {converted} Kilograms')
-elif unit == "K" or unit == "KG":
-    converted = weight / 0.45
-    print(f'You are {converted} pounds')
-else:
-    print('Invalid input')
-
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command == "start":
+        if started:
+            print("The car has already started.")
+        else:
+            started = True
+            print("Car Started")
+    elif command == "stop" :
+        if not started:
+            print("The car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print ("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("Sorry I do not understand that")
 
