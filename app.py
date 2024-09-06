7947dda361a776783662ac42eba3392b4f99d3bd
parisakh4 <126240144+parisakh4@users.noreply.github.com>
2024-09-06 12:24:55 +0200

    if practice

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-input('Enter your Weight')
+weight = int(input('Enter your Weight'))
+unit = input('(L)bs or (K)g').upper()
+
+if unit == "L"  or unit == "LBS" :
+    converted = weight * 0.45
+    print(f'You are {converted} Kilograms')
+elif unit == "K" or unit == "KG":
+    converted = weight / 0.45
+    print(f'You are {converted} pounds')
+else:
+    print('Invalid input')
+
+
